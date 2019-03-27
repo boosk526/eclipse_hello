c6b5bc4d284a24b18c63bcea0e7af02a7c0783c1
gyungbin <boosk526@naver.com>
2019-03-27 11:21:15 +0900

    create project create world's end dancehall

--- project0327/src/project0327/Helloworld.java
+++ project0327/src/project0327/Helloworld.java
@@ -1,6 +1,8 @@
 package project0327;
 
 public class Helloworld {
-	public static void main(String[] args) {System.out.println("Hello World");
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("World's End Dancehall");
 	}
 }
