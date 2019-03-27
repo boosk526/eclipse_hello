10850ae93cc083a0bbf602dd8a036f528dd7309c
gyungbin <boosk526@naver.com>
2019-03-27 11:50:18 +0900

    add print line

--- project0327/src/project0327/Helloworld.java
+++ project0327/src/project0327/Helloworld.java
@@ -5,6 +5,5 @@ public class Helloworld {
 		System.out.println("Hello World");
 		System.out.println("World's End Dancehall");
 		System.out.println("Earth's End Dancehall");
-		System.out.println("Hello Dance World");
 	}
 }
