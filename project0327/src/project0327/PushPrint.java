19d3594c28991108dd4ab75b90977e8f77a97061
boosk526 <48500714+boosk526@users.noreply.github.com>
2019-04-03 10:34:08 +0900

    Update PushPrint.java

--- project0327/src/project0327/PushPrint.java
+++ project0327/src/project0327/PushPrint.java
@@ -3,5 +3,6 @@ package project0327;
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Add Java Class File Push");
+		System.out.println("Wolrd with 42");
 	}
 }
