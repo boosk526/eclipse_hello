a3d9e63c4f557fdae6e8d5b0722f86d8ea5f6cdd
gyungbin <boosk526@naver.com>
2019-04-03 10:36:32 +0900

    github for git

--- project0327/src/project0327/PushPrint.java
+++ project0327/src/project0327/PushPrint.java
@@ -3,5 +3,7 @@ package project0327;
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Add Java Class File Push");
+		System.out.println("World with 43");
+		
 	}
 }
